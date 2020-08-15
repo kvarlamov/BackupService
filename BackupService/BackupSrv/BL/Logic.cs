466096d3771c8e846a1571500374b7bc8423d627
kvarlamov <k.varlamov88@yandex.ru>
2020-08-16 00:00:12 +0300

    1. Create Windows Service application. First steps to create logic

---
+++ BackupService/BackupSrv/BL/Logic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace BackupSrv.BL
+{
+    public class Logic
+    {
+        private static Timer _timer;
+        public static void Start()
+        {
+
+        }
+
+        private void StartBackup()
+        {
+            var folders = GetFoldersForBackup();
+            if (folders.Any())
+            {
+                //forEach directory
+                    //create directory with project name and date of creating
+                    //get files from source
+                    //copy files to destination
+            }
+        }
+
+        private List<string> GetFoldersForBackup()
+        {
+            return null;
+        }
+
+        private bool ClearOldData()
+        {
+            return true;
+        }
+    }
+}
